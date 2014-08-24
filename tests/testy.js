78d5ac95307f6a9d94449c53a7a454d50bebd6e0
emassalska <em92@wp.pl>
2014-08-24 23:44:55 +0200

    kolejna proba

--- tests/testy.js
+++ tests/testy.js
@@ -5,18 +5,12 @@ suite('submitMainPosts', function () {
     // (2) we can connect to the collection
     // (3) the collection is empty
     test('uczen moze sie zalogowac', function (done, server, client) {
-        server.eval(function () {
-            //dodajemy uczniow
-            var u1_id = Uczen.insert({
-                imie: "Karol",
-                nazwisko: "Kowalski"
-            });
-            addUser("Karol Kowalski", "u1@wp.pl", "uczen", "uczen", u1_id);
-        })
-
+   
         client.eval(function () {
-            Meteor.loginWithPassword('u1@wp.pl', 'uczen');
-            emit('zalogowany');
+            Meteor.loginWithPassword('u1@wp.pl', 'uczen', function() {
+                emit('zalogowany');
+            });
+            
         }).once('zalogowany', function () {
             assert.equal(Rola.findOne({ meteor_user: Meteor.userId() }).role, 'uczen');
             done();
