92a58518865b7c6d904a59f0684e2670c71fc269
emassalska <em92@wp.pl>
2014-08-24 23:25:55 +0200

    poprawka

--- tests/testy.js
+++ tests/testy.js
@@ -6,6 +6,11 @@ suite('submitMainPosts', function () {
     // (3) the collection is empty
     test('uczen moze sie zalogowac', function (done, server, client) {
         server.eval(function () {
+            //dodajemy uczniow
+            var u1_id = Uczen.insert({
+                imie: "Karol",
+                nazwisko: "Kowalski"
+            });
             addUser("Karol Kowalski", "u1@wp.pl", "uczen", "uczen", u1_id);
         })
 
